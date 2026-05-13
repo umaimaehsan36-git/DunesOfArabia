a7a04ab21990abce0e4e225ad8f55654bd13ac4c
ArsalanIrfan152 <Dailytv1469@gmail.com>
2026-05-14 02:10:36 +0500

    Created 2 pages destination and index Add destination rating model and database migration Update destination pages and add rating migration Implement destination ratings in UI and database

--- DunesOfArabia/Migrations/20260513204610_AddRatingToDestination.Designer.cs
+++ DunesOfArabia/Migrations/20260513204610_AddRatingToDestination.Designer.cs
@@ -4,6 +4,7 @@ using DunesOfArabia.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DunesOfArabia.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260513204610_AddRatingToDestination")]
+    partial class AddRatingToDestination
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
