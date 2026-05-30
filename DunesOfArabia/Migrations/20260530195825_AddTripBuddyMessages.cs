7d6daea5de29eaf792b74999f412236b7332e1d5
umaimaehsan36-git <umaimaehsan36@gmail.com>
2026-05-30 13:00:00 +0500

    Add AuthService JwtService AuthController and TripBuddy messages migration

---
+++ DunesOfArabia/Migrations/20260530195825_AddTripBuddyMessages.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DunesOfArabia.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTripBuddyMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1553913861-c0fddf2619ee?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1591604466107-ec97de577aff?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1577495508048-b635879837f1?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1623937491310-c3a62a8e2d76?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1590155259853-bf27a37d7e28?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1589881133595-a3c085cb731d?w=800");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1539667284076-a4d98d9ac42b?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1578662996442-48f60103fc96?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1558618666-fcd25c85cd64?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1486325212027-8081e485255e?w=800");
+        }
+    }
+}
