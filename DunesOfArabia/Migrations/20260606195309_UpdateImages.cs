384f8be700ab317a045b6131b9cac756ca813205
umaimaehsan36-git <umaimaehsan36@gmail.com>
2026-06-06 15:00:00 +0500

    Add update images migration

---
+++ DunesOfArabia/Migrations/20260606195309_UpdateImages.cs
@@ -0,0 +1,438 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DunesOfArabia.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9e37DVt4M1I211pOfT1IssjBND-9SgAR1lw&s");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVWjhrcr8ah0z-dcAqLMk3MdEJnyEeEnWA6g&s");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWUlbV_WI7gp3Y5NQJDTfg0bodkRsFqAeG5Q&s");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVWjhrcr8ah0z-dcAqLMk3MdEJnyEeEnWA6g&s");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "https://www.shutterstock.com/image-vector/saudi-heritage-site-gold-vector-260nw-1318947854.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUf9JsnsfrJ5RcwPQnIYRW1BPFC986jZ9kQA&s");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUf9JsnsfrJ5RcwPQnIYRW1BPFC986jZ9kQA&s");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp9j6JdWfpytu5Cvr0gcII_EnzAjDytQrWfA&s");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtlZLIubFvbZl_w46GxCD_MI3BKUdl5m3TsA&s");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVWjhrcr8ah0z-dcAqLMk3MdEJnyEeEnWA6g&s");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHsYjfCQ_ILm1-mvEe9MLGJrdpdBWb0a_4Ng&s");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVWjhrcr8ah0z-dcAqLMk3MdEJnyEeEnWA6g&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbkHeadrhYfuf22XpmKOUDUyNuDr45xy9LAg&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkr6s8yIzDplFvyaf7Wb_YzudYzaUO4W6NuA&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9e37DVt4M1I211pOfT1IssjBND-9SgAR1lw&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUf9JsnsfrJ5RcwPQnIYRW1BPFC986jZ9kQA&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUf9JsnsfrJ5RcwPQnIYRW1BPFC986jZ9kQA&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9e37DVt4M1I211pOfT1IssjBND-9SgAR1lw&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVWjhrcr8ah0z-dcAqLMk3MdEJnyEeEnWA6g&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVWjhrcr8ah0z-dcAqLMk3MdEJnyEeEnWA6g&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVWjhrcr8ah0z-dcAqLMk3MdEJnyEeEnWA6g&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHsYjfCQ_ILm1-mvEe9MLGJrdpdBWb0a_4Ng&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImageUrl",
+                value: "https://cdn.getyourguide.com/img/tour/e7c8f97da8eef2714cca139dc40b5a540a5905724396c527699f77150496bf79.jpg/68.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtlZLIubFvbZl_w46GxCD_MI3BKUdl5m3TsA&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3nMCnNECliwNqBgfMDmWlPNKmE3SF96KZAg&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp9j6JdWfpytu5Cvr0gcII_EnzAjDytQrWfA&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "ImageUrl",
+                value: "https://www.arabnews.pk/sites/default/files/styles/n_670_395/public/2022/05/13/3224821-1914193523.jpg?itok=hp-DS3F8");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHsYjfCQ_ILm1-mvEe9MLGJrdpdBWb0a_4Ng&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3nMCnNECliwNqBgfMDmWlPNKmE3SF96KZAg&s");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "ImageUrl",
+                value: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWUlbV_WI7gp3Y5NQJDTfg0bodkRsFqAeG5Q&s");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1616236197457-53e96373d0b0?w=900");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1509316785289-025f5b846b35?w=700");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?w=700");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1542401886-65d6c61db217?w=700");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1539667284076-a4d98d9ac42b?w=700");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1578662996442-48f60103fc96?w=700");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1591604466107-ec97de577aff?w=700");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1564769611905-cd27ee64e59b?w=700");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=700");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?w=700");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1446941303997-2843d7b4d20f?w=700");
+
+            migrationBuilder.UpdateData(
+                table: "Activities",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1519671282429-b44b0de7773e?w=700");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1586724237569-f3d0c1dee8c6?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1553913861-c0fddf2619ee?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1616236197457-53e96373d0b0?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1591604466107-ec97de577aff?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1591604466107-ec97de577aff?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1526392060635-9d6019884377?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1509316785289-025f5b846b35?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1519671282429-b44b0de7773e?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1542401886-65d6c61db217?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1577495508048-b635879837f1?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1623937491310-c3a62a8e2d76?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1559827260-dc66d52bef19?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1501854140801-50d01698950b?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1505118380757-91f5f5632de0?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1544551763-46a013bb70d5?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1590155259853-bf27a37d7e28?w=800");
+
+            migrationBuilder.UpdateData(
+                table: "Destinations",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "ImageUrl",
+                value: "https://images.unsplash.com/photo-1589881133595-a3c085cb731d?w=800");
+        }
+    }
+}
